1e9591cd335de34c025d8611ea36e89090c152ae
ansh5017 <89251057+ansh5017@users.noreply.github.com>
2021-08-20 15:38:37 +0530

    Update guess.java

--- guess.java
+++ guess.java
@@ -1,1 +1,45 @@
 
+import java.util.Scanner;
+
+class working
+{
+int random;
+   public  working()
+   {
+     random=(int)((Math.random())*100);
+
+   }
+
+   public void input(){
+       Scanner sc=new Scanner(System.in);
+      
+       int a=sc.nextInt();
+       if(a==random)
+       {
+           System.out.println("Bingo");
+           return;
+       }
+       else if (a>random)
+       {
+           System.out.println("Smaller");
+
+       }
+       else {
+           System.out.println("Bigger");
+       }
+        input();
+        return;
+
+   }
+
+}
+public class Guess {
+    public static void main(String[] args)
+     {
+         working condition=new working();
+         
+         condition.input();
+
+    }
+}
+
